ae4f8e11f9cb9b6edf16e3470823816f9668a931
Jeruko10 <al427528@uji.es>
2022-09-14 18:49:34 +0200

    Dec2Bin Arreglado

--- dec2bin.py
+++ dec2bin.py
@@ -7,7 +7,7 @@
 # ------------------------------------------------------
 def dec2bin(numero_decimal, numero_bits):
     numero_binario = bin(numero_decimal)
-    if numero_decimal >= 0
+    if numero_decimal >= 0:  #Los dos puntos! IMPORTANTE!
         numero_binario = numero_binario[2:len(numero_binario)]  #Quita el "0b" del principio
     
         while len(numero_binario) < numero_bits:      #Añade 0's a la izquierda si hace falta
